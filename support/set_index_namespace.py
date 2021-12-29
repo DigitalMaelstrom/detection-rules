7d862b57a058273beeb25dd3f28e8bd2ef818572
Jeff Stice-Hall <jsh@digitalmaelstrom.net>
2021-12-29 16:41:57 -0600

    Rewrite support/set_index_namespace script to use detection_rules pkg objects

--- support/set_index_namespace.py
+++ support/set_index_namespace.py
@@ -1,13 +1,21 @@
 from argparse import ArgumentParser
+import dataclasses as dc
 import logging
 from pathlib import Path
+import sys
+from typing import List
 
-import pytoml as toml
+this_path = Path().resolve()
+package_path = Path(*this_path.parts[:this_path.parts.index('detection-rules')+1])
+sys.path.append(str(package_path))
+
+from detection_rules.rule import QueryRuleData, TOMLRule
+from detection_rules.rule_loader import RuleCollection
 
 logger = logging.getLogger()
 
 
-def set_namespace_for_indices(namespace: str, root_path: str = "rules", **kwargs):
+def set_namespace_for_indices(namespace: str, root_path: str = "rules", backup=False, **kwargs):
     """
     Iterates across indices within each toml file found in subtree, modifying them to use namespaced indices.
     :param namespace: string namespace to add to index like `logs-*-<slug>*`
@@ -15,12 +23,12 @@ def set_namespace_for_indices(namespace: str, root_path: str = "rules", **kwargs
     :param kwargs:
     :return:
     """
-    for file in Path(root_path).rglob("*.toml"):
-        with open(file, 'w+') as fb:
-            data = toml.load(fb)
-            if 'rule' in data and 'index' in data['rule']:
-                data['rule']['index'] = list(map(lambda x: transform_index(x, namespace), data['rule']['index']))
-            toml.dump(data, file)
+    for rule in RuleCollection.default():
+        if isinstance(rule.contents.data, QueryRuleData):
+            replacement_rule = replace_index_field(rule,
+                                                   list(map(lambda x: transform_index(x, namespace),
+                                                            rule.contents.data.index)))
+            replacement_rule.save_toml()
 
 
 def transform_index(index, namespace):
@@ -37,6 +45,14 @@ def transform_index(index, namespace):
         return index
 
 
+# rule.contents.data.index
+def replace_index_field(rule: TOMLRule, value: List[str]):
+    data = dc.replace(rule.contents.data, index=value)
+    contents = dc.replace(rule.contents, data=data)
+    rule = dc.replace(rule, contents=contents)
+    return rule
+
+
 def main():
     """
     Main command line function
@@ -45,9 +61,10 @@ def main():
     parser = ArgumentParser()
     parser.add_argument("namespace", help='Namespace string to add to index pattern; e.g. logs-*-<namespace>*')
     parser.add_argument("--root_fs", default="rules", help='Path to root rule filesytem')
+    parser.add_argument("--backup", action='store_true', default=False, help='Modify rules after backing up originals')
     args = parser.parse_args()
 
-    set_namespace_for_indices(args.namespace, args.root_fs)
+    set_namespace_for_indices(args.namespace, root_path=args.root_fs, backup=args.backup)
 
 
 if __name__ == '__main__':
